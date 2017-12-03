02cd97f147836becf7f171ee216f90b4234b2a7b
onur hüseyin çantay <onurhuseyincantay@onurs-MacBook-Pro.local>
2017-12-03 13:19:59 +0300

    Added Custom Cell.xml icons and a toolbar

---
+++ app/src/main/java/com/example/onurhuseyincantay/myshoppingcart/Main.java
@@ -0,0 +1,32 @@
+package com.example.onurhuseyincantay.myshoppingcart;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class Main extends AppCompatActivity {
+    private ListView listView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+       myToolbar.setTitle(R.string.app_name);getSupportActionBar().setDisplayShowTitleEnabled(false);
+       listView = (ListView) findViewById(R.id.list_view);
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.toolbar, menu);
+        return true;
+    }
+
+
+}
