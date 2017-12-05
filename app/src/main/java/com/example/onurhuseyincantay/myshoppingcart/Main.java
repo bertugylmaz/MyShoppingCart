a42de74211166df59804a06ad5fe573a6fc4ac2a
Bertuğ <ylmazbertug@gmail.com>
2017-12-05 15:57:38 +0300

    addCart activity added

--- app/src/main/java/com/example/onurhuseyincantay/myshoppingcart/Main.java
+++ app/src/main/java/com/example/onurhuseyincantay/myshoppingcart/Main.java
@@ -1,10 +1,12 @@
 package com.example.onurhuseyincantay.myshoppingcart;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -15,18 +17,32 @@ public class Main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
-       myToolbar.setTitle(R.string.app_name);getSupportActionBar().setDisplayShowTitleEnabled(false);
-       listView = (ListView) findViewById(R.id.list_view);
+        myToolbar.setTitle(R.string.app_name);getSupportActionBar().setDisplayShowTitleEnabled(false);
+        listView = (ListView) findViewById(R.id.list_view);
 
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.toolbar, menu);
+
         return true;
     }
 
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()) {
+            case R.id.add_shopping_cart:
+                Intent addShoppingCartIntent = new Intent(Main.this, AddList.class);
+                startActivity(addShoppingCartIntent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
 }
