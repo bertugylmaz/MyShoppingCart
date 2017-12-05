4027f7cbaa886668b61226530f17493aae3eb6c4
Bertuğ <ylmazbertug@gmail.com>
2017-12-05 16:44:29 +0300

    Added dropdown menu data

--- app/src/main/java/com/example/onurhuseyincantay/myshoppingcart/AddList.java
+++ app/src/main/java/com/example/onurhuseyincantay/myshoppingcart/AddList.java
@@ -2,13 +2,23 @@ package com.example.onurhuseyincantay.myshoppingcart;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class AddList extends AppCompatActivity {
+
+    private String[] types={"Kg","Litre","Adet"};
+    private Spinner typeSpinner;
+    private ArrayAdapter<String> typesDataForSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_list);
 
+        typeSpinner = (Spinner)findViewById(R.id.typeSpinner);
+        typesDataForSpinner =  new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, types);
+        typesDataForSpinner.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        typeSpinner.setAdapter(typesDataForSpinner);
     }
 }
